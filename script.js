7a79546dc26272e70b742cb3f7fdfb140a285778
cpersichilli <caitlinpersichilli@gmail.com>
2017-10-20 13:01:11 -0400

    Updated Site
    Updated site

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $("#one").animate({
+            height:'toggle'
+        });
+    });
+    $("#button2").click(function(){
+        $("#two").animate({
+            height:'toggle'
+        });
+    });
+    $("#button3").click(function(){
+        $("#three").animate({
+            height:'toggle'
+        });
+    });
+    $("#button4").click(function(){
+        $("#four").animate({
+            height:'toggle'
+        });
+    });
+    $("#button5").click(function(){
+        $("#five").animate({
+            height:'toggle'
+        });
+    });
+});
